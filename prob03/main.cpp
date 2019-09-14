2802a082dfd814044dea9e9ff87288170cb61d52
mimidulog <mimidulog@cs104u-5g.acad.fullerton.edu>
2019-09-14 14:01:57 -0700

    Created Program to predict sales.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,18 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  long int my_sale2;
+  const double PERCENTAGE = 1.18;
+  const  long int FIRST_SALE = 2103419277;
+
+  std::cout << "Last's year's sale were $" << FIRST_SALE << std::endl;
+
+  my_sale2 = FIRST_SALE * PERCENTAGE;
+
+  std::cout << "This year's sales prediction: $" << my_sale2 << std::endl;
+
+  return 0;
+}
